0dc19957b03d5abd9e61a24a8438cba0c0c9698b
Evan Szymkowicz <evan.szymkowicz@evanwolf.tech>
2020-10-02 13:02:26 -0400

    config

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,7 +3,14 @@ import ErrorLayout from "../components/layout/error-layout"
 import image from "../../static/assets/404.svg"
 
 export default class NotFoundPage extends React.Component {
-	render() {
-		return <ErrorLayout code="404" image={image} title="Not Found" location={this.props.location} />
-	}
+  render() {
+    return (
+      <ErrorLayout
+        code="404"
+        image={image}
+        title="Not Found"
+        location={this.props.location}
+      />
+    )
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,61 @@
-import React from 'react';
-import { graphql } from 'gatsby';
+import React from "react"
+import {graphql} from "gatsby"
 
-import Story from '../components/sections/story';
-import Blog from '../components/sections/blog';
-import Featuring from '../components/sections/featuring';
-import Layout from '../components/layout/layout';
-import Section from '../components/ui/section';
+import Story from "../components/sections/story"
+import Blog from "../components/sections/blog"
+import Featuring from "../components/sections/featuring"
+import Layout from "../components/layout/layout"
+import Section from "../components/ui/section"
 
 export default class Index extends React.Component {
-	render() {
-		const { data } = this.props;
-		const posts = data.allMarkdownRemark.edges;
+  render() {
+    const {data} = this.props
+    const posts = data.allMarkdownRemark.edges
 
-		return (
-			<Layout location={this.props.location}>
-				<Story />
-				<Blog posts={posts} />
-				<Section>
-					<Featuring />
-				</Section>
-			</Layout>
-		);
-	}
+    return (
+      <Layout location={this.props.location}>
+        <Story />
+        <Blog posts={posts} />{" "}
+        <Section>
+          <Featuring />
+        </Section>{" "}
+      </Layout>
+    )
+  }
 }
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      limit: 3
+      sort: {fields: [frontmatter___date], order: DESC}
+      filter: {fields: {slug: {regex: "^/blog/"}}}
+    ) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD/MM/YYYY")
+            title
+            description
+            featuredImage {
+              childImageSharp {
+                sizes(maxWidth: 630) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`
